4274890ba4c97b9fd0d75a3ec483a63781411e47
Ryan Compere <rcompere@rightnow.org>
2018-10-23 18:12:03 -0500

    Pluralized folder name

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e
 
 dotnet restore --no-cache
 dotnet build --configuration Debug
-dotnet test ./test/OrderServiceDemo.Unit.Tests/OrderServiceDemo.Unit.Tests.csproj
+dotnet test ./tests/OrderServiceDemo.Unit.Tests/OrderServiceDemo.Unit.Tests.csproj
 dotnet publish ./src/OrderServiceDemo/OrderServiceDemo.csproj --output ../../publish/output
 
--- sonar.sh
+++ sonar.sh
@@ -9,12 +9,12 @@ then
     /d:sonar.organization="rightnow-ministries" \
     /d:sonar.host.url="https://sonarcloud.io" \
     /d:sonar.login=$SONAR_LOGIN \
-    /d:sonar.cs.opencover.reportsPaths="./test/OrderServiceDemo.Unit.Tests/coverage.opencover.xml" \
-    /d:sonar.coverage.exclusions="./test/**"
+    /d:sonar.cs.opencover.reportsPaths="./tests/OrderServiceDemo.Unit.Tests/coverage.opencover.xml" \
+    /d:sonar.coverage.exclusions="./tests/**"
 	
 	dotnet restore --no-cache
 	dotnet build --configuration Debug
-	dotnet test ./test/OrderServiceDemo.Unit.Tests/OrderServiceDemo.Unit.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover    
+	dotnet test ./tests/OrderServiceDemo.Unit.Tests/OrderServiceDemo.Unit.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
   
 	dotnet-sonarscanner end /d:sonar.login=$SONAR_LOGIN
 else
@@ -23,8 +23,8 @@ else
     /d:sonar.organization="rightnow-ministries" \
     /d:sonar.host.url="https://sonarcloud.io" \
     /d:sonar.login="cacbf3bb82d9f5f2be12f6e0fc19bcf6c2662f44" \
-    /d:sonar.cs.opencover.reportsPaths="./test/OrderServiceDemo.Unit.Tests/coverage.opencover.xml" \
-    /d:sonar.coverage.exclusions="./test/**" \
+    /d:sonar.cs.opencover.reportsPaths="./tests/OrderServiceDemo.Unit.Tests/coverage.opencover.xml" \
+    /d:sonar.coverage.exclusions="./tests/**" \
     /d:sonar.pullrequest.github.repository=$TRAVIS_REPO_SLUG \
     /d:sonar.pullrequest.base="development" \
     /d:sonar.pullrequest.provider="GitHub" \
@@ -33,7 +33,7 @@ else
 
 	dotnet restore --no-cache
 	dotnet build --configuration Debug
-	dotnet test ./test/OrderServiceDemo.Unit.Tests/OrderServiceDemo.Unit.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover    
+	dotnet test ./tests/OrderServiceDemo.Unit.Tests/OrderServiceDemo.Unit.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
   
 	dotnet-sonarscanner end /d:sonar.login="cacbf3bb82d9f5f2be12f6e0fc19bcf6c2662f44"
 fi;
